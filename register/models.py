d24a9bf4744c1c027fc0004ec39f8a56d96ee381
oluwaschunne <falopeoluwaseun2@gmail.com>
2024-07-08 22:28:17 +0100

    User model updated

--- register/models.py
+++ register/models.py
@@ -16,3 +16,25 @@ class UserManager(BaseUserManager):
         user.set_password(password)
         user.save(using=self.db)
         return user
+    
+    def create_superuser(self, email, firstName, lastName, password=None):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, firstName, lastName, password)
+
+
+class User(AbstractBaseUser):
+    userId = models.CharField(default=generate_uuid, unique=True, max_length=36, editable=False)
+    firstName = models.CharField(max_length=30)
+    lastName = models.CharField(max_length=30)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
+    phone = models.CharField(max_length=15, blank=True, null=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['firstName', 'lastName']
+
+    def __str__(self):
+        return self.email
