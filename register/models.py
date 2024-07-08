20253e4abcc98c02bd5a85f3942b8e3b513eff44
oluwaschunne <falopeoluwaseun2@gmail.com>
2024-07-08 22:17:33 +0100

    User model created

--- register/models.py
+++ register/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from .utils import generate_uuid
 
 # Create your models here.
+class UserManager(BaseUserManager):
+    def create_user(self, email, firstName, lastName, password=None):
+        if not email:
+            raise ValueError('The Email field must be set')
+        if not firstName:
+            raise ValueError('The First Name field must be set')
+        if not lastName:
+            raise ValueError('The last Name field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, firstName=firstName, lastname=lastName)
+        user.set_password(password)
+        user.save(using=self.db)
+        return user
