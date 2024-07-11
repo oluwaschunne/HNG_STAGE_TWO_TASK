74588bdc9bab0ef22aa7eb4812068583ac71dbb6
oluwaschunne <falopeoluwaseun2@gmail.com>
2024-07-11 12:24:29 +0100

    settings updated for deployment

--- hng_organisation/settings.py
+++ hng_organisation/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'default-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'True') == 'True'
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
