c1824c88d2b874ec92285db4ea7dd7d75e3d5e1f
oluwaschunne <falopeoluwaseun2@gmail.com>
2024-07-11 15:17:30 +0100

    updated

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('auth/register/', views.RegisterView.as_view(), name='user_registration'),
-    path('auth/login/', views.LoginView.as_view(), name='user_login'),
-    path('api/users/<uuid:userId>/', views.UserDetailView.as_view(), name='user_detail'),
-    path('api/organisations/', views.OrganisationListView.as_view(), name='organisation_list'),
-    path('api/organisations/<uuid:orgId>/', views.OrganisationDetailView.as_view(), name='organisation_detail'),
-    path('api/organisations/<uuid:orgId>/users/', views.AddUserToOrganisationView.as_view(), name='add_user_to_organisation'),
-    path('api/organisations/create/', views.CreateOrganisationView.as_view(), name='create_organisation'),
+    path('/auth/register', views.RegisterView.as_view(), name='user_registration'),
+    path('/auth/login', views.LoginView.as_view(), name='user_login'),
+    path('/api/users/<uuid:userId>', views.UserDetailView.as_view(), name='user_detail'),
+    path('/api/organisations', views.OrganisationListView.as_view(), name='organisation_list'),
+    path('/api/organisations/<uuid:orgId>', views.OrganisationDetailView.as_view(), name='organisation_detail'),
+    path('/api/organisations/<uuid:orgId>/users', views.AddUserToOrganisationView.as_view(), name='add_user_to_organisation'),
+    path('/api/organisations/create', views.CreateOrganisationView.as_view(), name='create_organisation'),
 ]
