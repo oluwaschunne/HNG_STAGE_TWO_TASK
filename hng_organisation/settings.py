ea174907688a12219ef0f5e80dc9d5eeb1f07065
oluwaschunne <falopeoluwaseun2@gmail.com>
2024-07-11 11:23:03 +0100

    vercel config added

--- hng_organisation/settings.py
+++ hng_organisation/settings.py
@@ -13,11 +13,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@@_*lkego!jh44tabbvjam0_c)8h-x99^31^o(1r7ly-q4f+xf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = [
-    'localhost',
-]
+ALLOWED_HOSTS = []
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
